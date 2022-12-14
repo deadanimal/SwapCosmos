d29638baba23b557eae35d0ee79cb37f3690896b
Afeez Aziz <afeezaziz@gmail.com>
2022-12-14 13:01:49 +0800

    123

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('offers', [WebController::class, 'list']);
+Route::get('offers/create', [WebController::class, 'create']);
+Route::post('offers', [WebController::class, 'store']);
+Route::get('offers/{uuid}', [WebController::class, 'detail']);
+Route::put('offers/{uuid}', [WebController::class, 'update']);
+
+Route::get('wallet', [WebController::class, 'home']);
+Route::get('calculator', [WebController::class, 'home']);
+Route::get('fees', [WebController::class, 'home']);
+Route::get('list-token', [WebController::class, 'home']);
+Route::get('privacy', [WebController::class, 'home']);
+Route::get('terms', [WebController::class, 'home']);
+
+Route::get('learn/{topic}', [WebController::class, 'home']);
